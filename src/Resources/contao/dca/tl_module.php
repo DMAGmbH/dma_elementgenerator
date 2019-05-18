cf7e558077655e615636db272fe3e9b281c27671
Alexander Doell <email@alexander-doell.de>
2019-05-18 22:26:31 +0200

    Create field dma_eg_data in tl_module

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,3 +1,11 @@
 <?php
 // Define callbacks
 $GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = array('DMAElementGeneratorCallbacks', 'module_onload');
+
+$GLOBALS['TL_DCA']['tl_dma_eg'] = array(
+    'fields' => array(
+        'dma_eg_data' => [
+            'sql' => "longtext NULL"
+        ],
+    )
+);
