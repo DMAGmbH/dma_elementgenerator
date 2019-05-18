228d8721d18dad890d347a353cbde53019a97b19
Thomas Lonnemann <thomas@conversoft.com>
2019-05-18 22:30:34 +0200

    tl_module

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,11 +1,6 @@
 <?php
 // Define callbacks
 $GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = array('DMAElementGeneratorCallbacks', 'module_onload');
-
-$GLOBALS['TL_DCA']['tl_dma_eg'] = array(
-    'fields' => array(
-        'dma_eg_data' => [
-            'sql' => "longtext NULL"
-        ],
-    )
+$GLOBALS['TL_DCA']['tl_module']['fields']['dma_eg_data'] = array(
+    'sql' => "longtext NULL"
 );
