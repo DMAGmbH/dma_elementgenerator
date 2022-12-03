1d1a9f73bdf296db32b78bb15f673a703c3be709
Janosch Oltmanns <kontakt@janosch-oltmanns.de>
2022-12-03 21:00:24 +0100

    Fixed Warnings in Install-Process

--- src/dma_elementgenerator/dca/tl_dma_eg_fields.php
+++ src/dma_elementgenerator/dca/tl_dma_eg_fields.php
@@ -100,7 +100,7 @@ $GLOBALS['TL_DCA']['tl_dma_eg_fields'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_dma_eg_fields']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.gif',
-				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+				'attributes'          => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\')) return false; Backend.getScrollOffset();"'
 			),
 			'show' => array
 			(
